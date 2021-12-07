10e225c62ff8fa5f2cf72344bff50c81be0930a9
gaohongmin <hongmin.gao@outlook.com>
2021-12-07 17:01:40 +0800

    package

---
+++ apis/src/hms/practice/4_package/greeting/greeting.go
@@ -0,0 +1,14 @@
+//Package greeting, contain two public method
+package greeting
+
+import "fmt"
+
+//Hello method
+func Hello() {
+	fmt.Println("Hello from package greeting!")
+}
+
+//Hi method
+func Hi() {
+	fmt.Println("Hi from package greeting!")
+}
---
+++ apis/src/hms/practice/4_package/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "apis/src/hms/practice/4_package/greeting"
+
+func main() {
+	greeting.Hello()
+	greeting.Hi()
+}
